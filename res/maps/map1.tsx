6c91dcb5cd34c8291f3d41dd84e0513f49de87ca
cristian <novacrez@gmail.com>
2018-12-14 13:34:32 -0300

    Fixed map references

--- res/maps/map1.tsx
+++ res/maps/map1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="32x32_map_tile v3.1 [MARGINLESS]" tilewidth="32" tileheight="32" tilecount="667" columns="23">
+<tileset version="1.2" tiledversion="1.2.1" name="map1" tilewidth="32" tileheight="32" tilecount="667" columns="23">
  <image source="map1.png" width="736" height="928"/>
 </tileset>
